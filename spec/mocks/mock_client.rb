905bcef473ee608dbfe2b74fc35512fb2b9ded2d
Brent Dillingham <brent@simplesis.com>
2008-07-28 19:05:00 -0400

    Moved MockClient into its own folder.

---
+++ spec/mocks/mock_client.rb
@@ -0,0 +1,55 @@
+# MockClient is my attempt to mock a client connection, which is normally
+# an instance of EventMachine::Connection. I'm overwriting the #send_data
+# method -- the method provided by EventMachine::Connection that actually
+# sends data to the client. Here I'm just capturing it in an output buffer so
+# that my tests can examine output easily.
+#
+# This may not be bestpractice at all and is subject to change.
+require 'colored'
+class MockClient
+  include Aeon::Client
+  
+  attr_reader :transcript, :input, :output
+
+  # the transcript is an associative array of input and output
+  #   [[:input, 'foo'], [:output, 'bar']]
+  def initialize
+    @transcript = []
+    @output     = []
+    @input      = []
+  end
+  
+  # Returns a string of a nicely formatted transcript. Width is 80 columns,
+  # and may explode all over the place if text is wider than that.
+  #
+  # Here's an example of what that looks like:
+  #
+  #   +==============================<( TRANSCRIPT )>====================================+
+  #   | What is your name, wanderer? > "TestPlayer\n"                                    |
+  #   | No player found by that name.                                                    |
+  #   |                                                                                  |
+  #   | What is your name, wanderer? >                                                   |
+  #   +==================================================================================+
+  #
+  def pretty_transcript
+    msg =  "\n+==============================<( TRANSCRIPT )>====================================+\n".white.bold
+    # 1. split at newlines with leading & trailing whitespace removed
+    # 2. pad each line to be 80 characters, encasing them inside our box
+    msg << @transcript.join.strip.split("\n").collect! {|e| "| #{e.ljust(80)} |"}.join("\n")
+    msg << "\n+==================================================================================+\n".white.bold
+    msg
+  end
+  
+  # add received data to the transcript as input
+  def receive_data(data)
+    @transcript << "#{data.inspect}".yellow
+    @input << data
+    super(data)
+  end
+  
+  # add received data to the transcript as output
+  def send_data(data)
+    @transcript << data
+    @output << data
+  end
+end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -8,52 +8,9 @@ require File.dirname(__FILE__) + '/../lib/aeon'
 require File.dirname(__FILE__) + '/matchers/output_matchers'
 include OutputMatchers
 
+# mocks
+require File.dirname(__FILE__) + '/mocks/mock_client'
+
 # Setup the test database using in-memory SQLite3
 require 'dm-core'
 DataMapper.setup(:default, 'sqlite3::memory:')
-
-
-# MockClient is my attempt to mock a client connection, which is normally
-# an instance of EventMachine::Connection. I'm overwriting the #send_data
-# method -- the method provided by EventMachine::Connection that actually
-# sends data to the client. Here I'm just capturing it in an output buffer so
-# that my tests can examine output easily.
-#
-# This may not be bestpractice at all and is subject to change.
-require 'colored'
-class MockClient
-  include Aeon::Client
-  
-  attr_reader :transcript, :input, :output
-
-  # the transcript is an associative array of input and output
-  #   [[:input, 'foo'], [:output, 'bar']]
-  def initialize
-    @transcript = []
-    @output     = []
-    @input      = []
-  end
-  
-  # Returns a string of a nicely formatted transcript. width is 80 columns.
-  def pretty_transcript
-    msg =  "\n+==============================<( TRANSCRIPT )>====================================+\n".white.bold
-    # 1. split at newlines with leading & trailing whitespace removed
-    # 2. pad each line to be 80 characters, encasing them inside our box
-    msg << @transcript.join.strip.split("\n").collect! {|e| "| #{e.ljust(80)} |"}.join("\n")
-    msg << "\n+==================================================================================+\n".white.bold
-    msg
-  end
-  
-  # add received data to the transcript as input
-  def receive_data(data)
-    @transcript << ">#{data.inspect}".yellow
-    @input << data
-    super(data)
-  end
-  
-  # add received data to the transcript as output
-  def send_data(data)
-    @transcript << data
-    @output << data
-  end
-end
