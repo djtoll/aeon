ffe01aa4a76e4289b064f9810d8ac8f77e57a4b6
Brent Dillingham <brent.dillingham@gmail.com>
2008-08-22 00:35:45 -0400

    Added script/console ala Rails' console mode.

--- rakefile.rb
+++ rakefile.rb
@@ -5,11 +5,6 @@ task :db do
   exec "sqlite3 db/db.sqlite3"
 end
 
-desc "Does 'irb -r lib/aeon.rb' for console mode a la Rails"
-task :console do
-  exec "irb -r lib/aeon.rb"
-end
-
 namespace :db do
   desc "Invokes DataMapper.auto_migrate!"
   task :migrate do
